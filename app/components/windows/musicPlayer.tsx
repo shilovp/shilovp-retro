53e7d55e3a894c79c4c7a513daba4c680ce91fb6
Pavel Shilov <p.shilov>
2024-07-12 19:43:22 +0200

    doubleclick logic added for icon

---
+++ app/components/windows/musicPlayer.tsx
@@ -0,0 +1,10 @@
+const About: React.FC<{}> = () => {
+    return (
+        <div>
+            <p>player</p>
+        </div>
+    )
+}
+
+
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import DraggableWindow from "./components/draggableWindow";
 import Start from "./components/start";
 import About from "./components/windows/about";
 import MovableIcon from "./components/movableIcon";
+import MusicPlayer from "./components/windows/musicPlayer";
 
 export default function Home() {
 
@@ -11,6 +12,8 @@ export default function Home() {
   const [isAboutHidden, setIsAboutHidden] = useState(false);
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
   const [isSettingsHidden, setIsSettingsHidden] = useState(false);
+  const [isMusicPlayerOpen, setIsMusicPlayerOpen] = useState(false);
+  const [isMusicPlayerHidden, setIsMusicPlayerHidden] = useState(false);
 
   const onStartMenuClick = (menu: string) => {
     if (menu === 'about') {
@@ -44,24 +47,32 @@ export default function Home() {
         ) : (
           ''
         )}
+        {isMusicPlayerOpen && !isMusicPlayerHidden ? (
+          <DraggableWindow title="Music player" onClose={() => { setIsMusicPlayerOpen(false); setIsMusicPlayerHidden(false) }} onHide={() => setIsSettingsHidden(true)}>
+            <MusicPlayer></MusicPlayer>
+          </DraggableWindow>
+          // Use special separate component here for music player
+        ) : (
+          ''
+        )}
         <MovableIcon
           icon={'./play.jpg'}
           label="Music"
-          initialPosition={{x: 25, y: 70}}
+          initialPosition={{ x: 25, y: 70 }}
+          onDoubleClick={() => { setIsMusicPlayerOpen(true); setIsMusicPlayerHidden(false) }}
+        />
+        {/* <MovableIcon
+          icon={'./play.jpg'}
+          label="Music"
+          initialPosition={{ x: 90, y: 70 }}
           onDoubleClick={() => { }}
         />
         <MovableIcon
           icon={'./play.jpg'}
           label="Music"
-          initialPosition={{x: 90, y: 70}}
+          initialPosition={{ x: 155, y: 70 }}
           onDoubleClick={() => { }}
-        />
-        <MovableIcon
-          icon={'./play.jpg'}
-          label="Music"
-          initialPosition={{x: 155, y: 70}}
-          onDoubleClick={() => { }}
-        />
+        /> */}
       </div>
     </main>
   );
